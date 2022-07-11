8f7643686e6f457b7f4b4b6babc6b65ffbb2f2a1
Molly Trepanier <mtrepanier123@gmail.com>
2022-07-11 16:20:23 -0700

    cleaned up poorly written first try

--- src/App.js
+++ src/App.js
@@ -1,68 +1,53 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import About from "./components/about";
+import Portfolio from "./components/portfolio";
+import Home from "./components/home";
+
 
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-              <Link to="/about">About</Link>
-              <Link to="/portfolio">Portfolio</Link>
-            </li>
-          </ul>
-        </nav>
-        {/* <Routes>
-          <Route path="/about">
-            {about()}
-          </Route>
-          <Route path="/portfolio">
-            {portfolio()}
-          </Route>
-          <Route path="/">
-            {home()}
-          </Route>
-        </Routes> */}
-      </div>
-      </Router>
+    <nav>
+      <Home />
+      <About />
+      <Portfolio />
+    </nav>
   );
 }
-
-// const home = () => {
-//   return <h2>Home</h2>
+// function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/about">About</Link>
+//             </li>
+//             <li>
+//               <Link to="/portfolio">Portfolio</Link>
+//             </li>
+//           </ul>
+//         </nav>
+//         {/* <Routes>
+//           <Route path="/about">
+//             {about()}
+//           </Route>
+//           <Route path="/portfolio">
+//             {portfolio()}
+//           </Route>
+//           <Route path="/">
+//             {home()}
+//           </Route>
+//         </Routes> */}
+//       </div>
+//     </Router>
+//   );
 // }
-// const portfolio = () => {
-//   return <h2>Portfolio</h2>
-// }
-// const about = () => {
-//   return <h2>i like cheese</h2>
-// }
-/***
- * function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
- */
 
 export default App;
